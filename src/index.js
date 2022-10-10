2b1b3e85b7b35bb96ba112f03abb895c899c9c14
elianmedina <elian.medina@uao.edu.co>
2022-10-10 05:04:03 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter,Link,Navigate,NavLink,Route,Routes } from 'react-router-dom'
 import { Principal } from './paginaPrincipal/component/Principal';
 import { Tiendas } from './paginaTiendas/Tiendas';
 import { Comidas } from './paginaComida/component/Comidas';
@@ -20,7 +20,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Carrito />
+
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
